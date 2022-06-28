179b01b607f56270ce767c274651a8593d251f17
João Filho <joao.filho.g3@gmail.com>
2022-06-28 18:59:22 -0300

    Testando modelo preditivo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,14 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from datetime import datetime
-from pycaret.regression import *
+from flask import Flask
+from flask_restful import Resource, Api
 
-loaded_model = load_model('my_model')
+app = Flask(__name__)
+api = api(app)
 
-def realizar_predicao(loaded_model, temp, cond, sal, depth, ph, turbid, chl, odo_sat, data_hora):
-  data_hora = pd.to_datetime(data_hora)
+class DissolvedOxygen(Resource):
+    def get(self):
+        return {
+            "teste": "teste"
+        }
 
-  dia_da_semana = [datetime.weekday(data_hora)][0]
-  ano = [data_hora.year][0]
-  mes = [data_hora.month][0]
-  dia_do_mes = [data_hora.day][0]
-  hora = [data_hora.hour][0]
-  minutos = [data_hora.minute][0]
-  semana = [data_hora.week][0]
-  trimestre = [data_hora.quarter][0]
-
-  col_names = ['temp_c','cond_us','sal_ppt','depth_m','pH ','turbid_ntu','chl_ug/l','odo_sat_%','dia_da_semana','ano','mes','dia_do_mes','hora','minutos','semana','trimestre']
-  sample_values = [temp, cond, sal, depth, ph, turbid, chl, odo_sat, dia_da_semana, ano, mes, dia_do_mes, hora, minutos, semana, trimestre]
-
-  d = dict(zip(col_names, sample_values))
-  unseen_data = pd.DataFrame([d])
-
-  predictions = predict_model(loaded_model, data=unseen_data)
-
-  return predictions.head()['Label'][0]
-  
-valor = realizar_predicao(
-            loaded_model,
-            temp = 34.49,
-            cond = 84.0,
-            sal = 0.04,
-            depth = 0.404,
-            ph = 0.0,
-            turbid = -28.6,
-            chl = -0.57,
-            odo_sat = 100.7,
-            data_hora = "2020-11-21 12:21:00")
-
-st.json({
-    'predicao': float(valor)
-})
+api.add_resource(DissolvedOxygen, "/do")
